95b2136e2bef00a4c3d215194a82a926ee493bb1
Chemutai Kevin <kchemutai@gmail.com>
2024-01-19 01:33:45 -0700

    Initial Customization

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles";
-import VinayakSingh from "../../assets/VinayakSingh.png";
+import KevinChemutai from "../../assets/profile_final.png";
 import wordpress from "../../assets/wordpress.svg";
 import shopify from "../../assets/shopify.svg";
 import htmlIcon from "../../assets/html-icon.svg";
@@ -12,92 +12,106 @@ import vueIcon from "../../assets/vue-icon.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
-
 export function About() {
-  return (
-    <Container id="about">
-      <div className="about-text">
-        <ScrollAnimation animateIn="fadeInLeft">
-          <h2>About me</h2>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
-          <p>
-          Hi there! I'm Vinayak, a website developer with a passion for creating custom online experiences for my clients. With a skill set including HTML, CSS, JavaScript, and React, I have the tools to bring any website vision to life.
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-          <p>
-          But my services go beyond just custom development - I'm also proficient in using CMS systems like WordPress and Shopify, making it easy for my clients to take control of their own websites and keep them up to date.</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
-          <p>
-          So if you're in need of a new website or just looking to revamp your current online presence, I'd love to chat and see how I can help. Let's bring your website dreams to reality together!</p>
-        </ScrollAnimation>
+	return (
+		<Container id="about">
+			<div className="about-text">
+				<ScrollAnimation animateIn="fadeInLeft">
+					<h2>About me</h2>
+				</ScrollAnimation>
+				<ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+					<p>
+						Hi there! I'm Kevin, a Software Engineer with a passion for creating
+						software solutions that tranform the world. With a skill set including
+						fronend languages HTML, CSS, JavaScript, React, Angular, backend languages
+						Java, Python I have the tools to bring any Web Development ideas vision to
+						life.
+					</p>
+				</ScrollAnimation>
+				<ScrollAnimation
+					animateIn="fadeInLeft"
+					delay={0.2 * 1000}
+					style={{ marginTop: "2rem", marginBottom: "2rem" }}
+				>
+					<p>
+						But my services go beyond just development - I'm also proficient in using
+						Automation tools and Cloud Technologies like Jenkins, AWS and GCP making
+						it easy for deployments and cost saving by taking advantage of on demand
+						delivery of computer power & storage.
+					</p>
+				</ScrollAnimation>
+				<ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+					<p>
+						So if you're in need of a Software Engineer to help to digitalize your
+						current business workflows and boost sales and online presence, I'd love
+						to chat and see how I can help. Let's bring your dreams to reality
+						together!
+					</p>
+				</ScrollAnimation>
 
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
-          <h3>Here are my main skills:</h3>
-        </ScrollAnimation>
-        <div className="hard-skills">
+				<ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+					<h3>Here are my main skills:</h3>
+				</ScrollAnimation>
+				<div className="hard-skills">
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
+							<img src={wordpress} alt="Wordpress" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+							<img src={shopify} alt="shopify" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+							<img src={reactIcon} alt="React" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+							<img src={typescriptIcon} alt="Typescript" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
+							<img src={vueIcon} alt="Vue" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+							<img src={nodeIcon} alt="Node" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+							<img src={htmlIcon} alt="Html" />
+						</ScrollAnimation>
+					</div>
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
-              <img src={wordpress} alt="Wordpress" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
-              <img src={shopify} alt="shopify" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={reactIcon} alt="React" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
-              <img src={typescriptIcon} alt="Typescript" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
-              <img src={vueIcon} alt="Vue" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
-              <img src={nodeIcon} alt="Node" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
-              <img src={htmlIcon} alt="Html" />
-            </ScrollAnimation>
-          </div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+							<img src={cssIcon} alt="Css" />
+						</ScrollAnimation>
+					</div>
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
-              <img src={cssIcon} alt="Css" />
-            </ScrollAnimation>
-          </div>
-
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={boostrapIcon} alt="bootstrap" />
-            </ScrollAnimation>
-          </div>
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={jsIcon} alt="JavaScript" />
-            </ScrollAnimation>
-          </div>
-        </div>
-      </div>
-      <div className="about-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
-          <img src={VinayakSingh} alt="Vinayak Singh" />
-        </ScrollAnimation>
-      </div>
-    </Container>
-  )
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+							<img src={boostrapIcon} alt="bootstrap" />
+						</ScrollAnimation>
+					</div>
+					<div className="hability">
+						<ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+							<img src={jsIcon} alt="JavaScript" />
+						</ScrollAnimation>
+					</div>
+				</div>
+			</div>
+			<div className="about-image">
+				<ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
+					<img src={KevinChemutai} alt="Vinayak Singh" />
+				</ScrollAnimation>
+			</div>
+		</Container>
+	);
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,65 +1,56 @@
-import { Container } from './styles'
+import { Container } from "./styles";
 
-import reactIcon from '../../assets/react-icon.svg'
-import linkedin from '../../assets/linkedin.svg'
-import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
-import telegram from '../../assets/telegram.svg'
-import instagramIcon from '../../assets/instagram.svg'
+import reactIcon from "../../assets/react-icon.svg";
+import linkedin from "../../assets/linkedin.svg";
+import githubIcon from "../../assets/github.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+import telegram from "../../assets/telegram.svg";
+import instagramIcon from "../../assets/instagram.svg";
 // import discordIcon from '../../assets/discord.png'
 
 export function Footer() {
-  return (
-    <Container className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.vinayak</span>
-        <span>singh.in</span>
-      </a>
-      <div>
-        <p>
-          This Website was made with <img src={reactIcon} alt="React" />
-          {/* <span>❤️</span> */}
-        </p>
-      </div>
+	return (
+		<Container className="footer">
+			<a href="https://vinayaksingh.in" className="logo">
+				<span>www.kevin</span>
+				<span>chemutai.com</span>
+			</a>
+			<div>
+				<p>
+					This Website was made with <img src={reactIcon} alt="React" />
+					{/* <span>❤️</span> */}
+				</p>
+			</div>
 
-      <div className="social-media">
-        <a
-          href="https://www.linkedin.com/in/codevinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedin} alt="Linkedin" />
-        </a>
-        <a
-          href="https://github.com/CodeVinayak/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-        </a>
-        <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a>
-        <a
-          href="https://www.instagram.com/vinayaksingh.in"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={instagramIcon} alt="Instagram" />
-        </a>
-     
-      </div>
-    </Container>
-  )
+			<div className="social-media">
+				<a
+					href="https://www.linkedin.com/in/kevinchemutai"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img src={linkedin} alt="Linkedin" />
+				</a>
+				<a href="https://github.com/kchemutai/" target="_blank" rel="noreferrer">
+					<img src={githubIcon} alt="GitHub" />
+				</a>
+				<a
+					href="https://api.whatsapp.com/send/?phone=%2B641239752&text=Hello+Kevin"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img src={whatsapp} alt="Whatsapp" />
+				</a>
+				<a href="https://t.me/CodeVinayak" target="_blank" rel="noreferrer">
+					<img src={telegram} alt="telegram" />
+				</a>
+				<a
+					href="https://www.instagram.com/kchemutai"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<img src={instagramIcon} alt="Instagram" />
+				</a>
+			</div>
+		</Container>
+	);
 }
